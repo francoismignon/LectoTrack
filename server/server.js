2fec1cc9f6017cfccc79936958483feb3fa76577
Mignon François <mignon.francois@gmail.com>
2025-06-04 13:39:53 +0200

    Extract AuthService (BL)

--- server/server.js
+++ server/server.js
@@ -1,53 +1,28 @@
 const express = require('express');
 const env = require('dotenv');
 const db = require('./src/models');
-const bcrypt = require('bcrypt');
 const UserRepository = require('./src/Repositories/UserRepository');
+const AuthService = require('./src/services/AuthService');
 
 
 env.config();
 const app = express();
 app.use(express.json());//pour lire le requ.body en JSON
 const PORT = process.env.PORT;
-const saltRound = 10; //nbr de salage
 const userRepository = new UserRepository(db.User);
+const authService = new AuthService(userRepository);
 
 
 //Routes
 app.post("/api/v1/auth/register", async (req, res)=>{
     const {login, password, confirmPassword} = req.body;
-    //formater correctement l'entrée (BL)
-    const loginFormatted = login.slice(0,1).toUpperCase() + login.slice(1).toLowerCase();
     
     try {
-  
-        const existingUser = await userRepository.findByLogin(loginFormatted);
-        
-        if(existingUser){
-            return res.status(400).json({ message: "L'utilisateur existe déja" });
-        }
-
-        //Verifier si le mot de passe correspont a la verification(BL)
-        if(password !== confirmPassword){
-            return res.status(400).json({message:"Les mot de passe ne correpondent pas"});
-        }
-        //Hachage (BL)
-        const hashPassword = await bcrypt.hash(password, saltRound);
-    
-        const newUser = await userRepository.create({
-            login: loginFormatted,
-            password: hashPassword
-        });
-
-        res.status(201).json({ 
-            user: {
-                id:newUser.id,
-                login:newUser.login,
-                roleId:newUser.roleId
-        }});
+        const newUser = await authService.create(login, password, confirmPassword); 
+        res.status(201).json(newUser);
 
     } catch (error) {
-        console.log(error.message);
+        res.status(error.statusCode || 500).json({error:error.message});
     } 
 });
 
--- server/src/services/AuthService.js
+++ server/src/services/AuthService.js
@@ -1,7 +1,45 @@
+const bcrypt = require('bcrypt');
+
 class AuthService{
     
     constructor(userRepository){
         this.userRepository = userRepository;
+    }
+    
+    async create(login, password, confirmPassword  ){
+        const saltRound = 10;
+        //formater correctement l'entrée (BL)
+        const loginFormatted = login.slice(0,1).toUpperCase() + login.slice(1).toLowerCase();
+
+        //Verifie si l'utilisateur existe déja
+        const existingUser = await this.userRepository.findByLogin(loginFormatted);
+        
+        if(existingUser){
+            //Creation manuel d'un code d'erreur, il sera recupere dans le try/catch pour pouvoir renvoyer correctement un message json
+            const err = new Error("L'utilisateur existe déja");
+            err.statusCode = 409;
+            throw err;
+        }
+
+        //Verifier si le mot de passe correspont a la verification(BL)
+        if(password !== confirmPassword){
+            const err = new Error("Les mots de passe ne correpondent pas");
+            err.statusCode = 401;
+            throw err;
+        }
+        //Hachage (BL)
+        const hashPassword = await bcrypt.hash(password, saltRound);
+        //creation de l'utilisateur (BL)
+        const newUser = await this.userRepository.create({
+            login: loginFormatted,
+            password: hashPassword
+        });
+        //on ne retourne que le necessaire
+        return ({
+            id:newUser.id,
+            login:newUser.login,
+            roleId:newUser.roleId
+        });
     }
 }
 module.exports = AuthService;
