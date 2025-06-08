aa6bd646122b80eff3f4efff1959038e06dbeb4f
Mignon François <mignon.francois@gmail.com>
2025-06-08 21:16:20 +0200

    Route GET readings/:id/comments OK

--- server/server.js
+++ server/server.js
@@ -11,6 +11,46 @@ app.use(express.json());//pour lire le requ.body en JSON
 const PORT = process.env.PORT;
 
 
+app.get("/api/v1/readings/:id/comments", checkTokenJwt, async (req, res) => {
+    try {
+        //les information dont j'ai besoin : couvetur, titre, auteur, liste des commentaires pour cette lecture
+        // sur cette vue j'ai juste besoin d'afficher la liste des commentaires de la lecture
+        const { id: userId } = req.user;
+        const { id: idReading } = req.params;
+
+        const comments = await db.Reading.findAll({
+            where: {
+                id: idReading,
+                userId
+            },
+            attributes: ['progress'],
+            include: [
+                {
+                    model: db.Comment,
+                    as: 'comments',
+                    attributes: ['pageNbr', 'content']
+                },
+                {
+                    model: db.Book,
+                    as: 'book',
+                    attributes: ['coverUrl', 'title'],
+                    include:[
+                        {
+                            model:db.Author,
+                            as:'author',
+                            attributes:['name']
+                        }
+                    ]
+                }
+            ]
+
+        });
+        res.status(200).json(comments);
+    } catch (error) {
+        res.status(error.statusCode || 500).json({ error: error.message });
+    }
+});
+
 //Route Auth
 app.use("/api/v1/auth", authRoutes);
 //Route Readings
